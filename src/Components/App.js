9d8059a955704dee65e244c3fdd761c6b3828c0f
delcidj22 <jdelcid23@gmail.com>
2022-07-24 14:49:20 -0700

    Update SRC files, make updates for testing

---
+++ src/Components/App.js
@@ -0,0 +1,14 @@
+//import logo from './logo.svg';
+import './App.css';
+//import Home from './Home';
+
+function App(){
+  return ( 
+    //<React.Fragment>
+      //<Header />
+      <h1> Testing </h1>
+    //</React.Fragment>
+  );
+}
+
+export default App;
